50fab4e2683152d7effd98b51ece89517082495a
dylz555444999 <dylz555444999@gmail.com>
2025-03-23 18:11:35 +0000

    Added varibles to the script inside

--- Assets/Cube.cs
+++ Assets/Cube.cs
@@ -2,15 +2,19 @@ using UnityEngine;
 
 public class Cube : MonoBehaviour
 {
+    int numberOfTimes = 5;
+    string nameOfTheKey = "ENTER";
+    float speedOfBraking = 6.94f;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         Debug.Log("If you press the up arrow you'll JUMP!");
-        Debug.Log("If you press the left or right arrow you'll MOVE!");
+        Debug.Log("If you press the left or right arrow" + " " + numberOfTimes + " " + "you'll MOVE!");
 
-        Debug.LogWarning("This is a warning message");
+        Debug.LogWarning("if you press the" + " " +  nameOfTheKey + " " + "nothing happens");
 
-        Debug.LogError("This is an error message");
+        Debug.LogError("your speed is" + " " + speedOfBraking);
     }
 
     // Update is called once per frame
