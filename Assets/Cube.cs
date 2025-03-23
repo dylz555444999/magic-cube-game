a5a38e5c017ba0685ed9a86d9feaa99c280992af
dylz555444999 <dylz555444999@gmail.com>
2025-03-23 18:32:22 +0000

    Added arrow key pressed detection

--- Assets/Cube.cs
+++ Assets/Cube.cs
@@ -9,17 +9,31 @@ public class Cube : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Debug.Log("If you press the up arrow you'll JUMP!");
-        Debug.Log("If you press the left or right arrow" + " " + numberOfTimes + " " + "you'll MOVE!");
+        //Debug.Log("If you press the up arrow you'll JUMP!");
+       // Debug.Log("If you press the left or right arrow" + " " + numberOfTimes + " " + "you'll MOVE!");
 
-        Debug.LogWarning("if you press the" + " " +  nameOfTheKey + " " + "nothing happens");
+       // Debug.LogWarning("if you press the" + " " +  nameOfTheKey + " " + "nothing happens");
 
-        Debug.LogError("your speed is" + " " + speedOfBraking);
+       // Debug.LogError("your speed is" + " " + speedOfBraking);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            print("up arrow key is pressed");
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            print("down arrow  key is pressed");
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            print("spacebar key is pressed");
+        }
     }
 }
